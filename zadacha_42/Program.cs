ccf4c83531dfaa28a72aaa6d28c3a79473b19dd9
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-14 17:46:08 +0300

    Positive numbers

---
+++ zadacha_42/Program.cs
@@ -0,0 +1,35 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+void FillArray( int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" Введите {i +1}е  число:  ");
+        array[i] = int.Parse(Console.ReadLine()?? "0");
+    }
+}
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} " );
+    }
+}
+int PositiveNumbers(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+            count++;
+    }
+    return count;
+}
+Console.Write("Введите количество вводимых чисел  ");
+int length = int.Parse(Console.ReadLine()??"0");
+int[] SpecifiedArray = new int[length];
+FillArray(SpecifiedArray);
+Console.WriteLine();
+PrintArray(SpecifiedArray);
+Console.WriteLine();
+int number = PositiveNumbers(SpecifiedArray);
+Console.WriteLine($" Количество чисел больше нуля равно {number} ");
