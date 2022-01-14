ef2cc7df744a8b0230df3f8e990af1fd7c078857
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-14 16:58:31 +0300

    triangle

---
+++ zadacha_41/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить являются ли три числа сторонами треугольника
+Console.Write("Введите число #1  ");
+int numberA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число #2  ");
+int numberB = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число #3 ");
+int numberC = int.Parse(Console.ReadLine()??"0");
+if ((numberA + numberB > numberC) && (numberA + numberC > numberB) && (numberB + numberC > numberA))
+     Console.WriteLine("да");
+else Console.WriteLine("нет");
+
